42450df7d3c73a772eecfa723ee870f36dc98836
Dalton Mills <dmills@sourceallies.com>
2016-01-10 12:25:15 -0600

    index page now uses ejs

--- source/serve.js
+++ source/serve.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var app = express();
 
-//app.engine('html', require('ejs').renderFile);
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views');
 
 var router = express.Router();
 
-app.use('/', express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/public/css'));
-//app.use('/static', express.static(__dirname + '/public'));
-
-
 router.get('/at/:room', function(req, res) {
     var room = req.params.room;
-    res.render('room/index', { name : room });
+    res.render('room/index', { room : room });
 });
 
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.use('/', express.static(__dirname + '/public'));
 app.use('/', router);
 
 app.listen(3000);
